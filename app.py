1b6456ece500f84fc92c13fa76bdabbb0e925322
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-04-14 12:03:20 -0300

    agregamos 2 rutas mas con sqlite

--- app.py
+++ app.py
@@ -1,6 +1,56 @@
 from flask import Flask
+import sqlite3 
 
 app = Flask(__name__)
+
+def dict_factory(cursor, row):
+   """Arma un diccionario con los valores de la fila."""
+   fields = [column[0] for column in cursor.description]
+   return {key: value for key, value in zip(fields, row)}
+
+db = None
+def abrirConexion():
+    global db
+    db = sqlite3.connect("instance/datos.sqlite")
+    db.row_factory = dict_factory 
+
+def cerrarConexion():
+    global db
+    db.close()
+    db = None
+
+@app.route('/test-db')
+def testDB():
+    abrirConexion()
+    cursor = db.cursor()
+    cursor.execute("SELECT COUNT(*) AS cant FROM usuarios ;")
+    res =cursor.fetchone()
+    registros = res ["cant"]
+    cerrarConexion()
+    return f"Hay {registros} registros en la tabla de ususario"
+
+@app.route("/crear-usuario")
+def testCrear():
+    nombre = "leandro"
+    email = "leandro@etec.uba.ar"
+    abrirConexion()
+    cursor = db
+    consulta = "INSERT INTO usuarios(usuario, email)VALUES (?,?);"
+    cursor.execute(consulta,(nombre,email))
+    db.commit()
+    cerrarConexion()
+    return f"Registro agregado({nombre})"
+
+
+@app.route("/crear-usuario/<string:nombre>/<string:email>")
+def testCrearXArgumento(nombre,email):
+    abrirConexion()
+    cursor = db
+    consulta = "INSERT INTO usuarios(usuario, email)VALUES (?,?);"
+    cursor.execute(consulta,(nombre,email))
+    db.commit()
+    cerrarConexion()
+    return f"Registro agregado({nombre},{email})"
 
 
 @app.route('/saludar/hola')
@@ -15,3 +65,4 @@ def chau():
 def sum(n1,n2):
     s = n1+n2
     return f'<p>  {n1}+{n2}={s} </p>'
+
